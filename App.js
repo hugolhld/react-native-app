f58cb35275154b6672bc31903d75fe95e75adb4b
Lucien Chotard <61746435+chotardlucien@users.noreply.github.com>
2021-01-05 16:07:28 +0100

    listItem

--- App.js
+++ App.js
@@ -2,10 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, InputText } from 'react-native';
 import Search from './src/components/Search'
+import ListItem from './src/components/ListItem'
 
 export default function App() {
   return (
+    <>
       <Search />
+      <ListItem img={require("./src/img/sampleitem.jpg")} title="KillBill" date="26/11/2003" />
+    </>
   );
 }
 
